b7d0e5502083ade08e30cbe0d4e909946f298246
sibei-git <3139605017@qq.com>
2025-11-10 17:23:15 +0000

    11.10

--- proj3.r
+++ proj3.r
@@ -1,5 +1,7 @@
+
+
 # task1
-par(mfrow=c(1,2))
+
 library(splines)
 
 #probability of each fatal disease duration(pi(j))
@@ -7,8 +9,8 @@ d <- 1:80;edur <- 3.151;sdur <- .469
 pd <- dlnorm(d,edur,sdur);
 pd <- pd/sum(pd)
 
-#create Xbar_X_S
-Xbar_X_S <- function(t,K=80,L=80,L0=30){
+#create Xtilde_X_S
+Xtilde_X_S <- function(t,K=80,L=80,L0=30){
   t <- as.numeric(t)
   n <- length(t)
   
@@ -43,11 +45,6 @@ Xbar_X_S <- function(t,K=80,L=80,L0=30){
   list(Xtilde = Xtilde, X = X, S = S, tf = tf, knots = knots, pi = pd)
 }
 
-##data
-data <- read.table('/Users/kangxinwei/sds/statistical programming/Assessment3_Group10/engcov .txt', header = TRUE)
-t_obs <- data$julian
-y <- data$nhs
-out<-Xbar_X_S(t_obs)
 
 #task 2
 negloglik_penalty <- function(gamma, X, y, S, lambda) {
@@ -70,59 +67,204 @@ grad_negloglik_penalty <- function(gamma, X, y, S, lambda){
   return(grad_gamma)
 }
 
-# xga <- rgamma(K)
-# ga0 <- negloglik_penalty(xga, X, y, S, lambda = 5e-5)
-# ga1 <- rep(0,K)
-# eps <- 1e-8
-# for (i in 1:K){
-#   xa_0 <- xga
-#   xa_0[i] <- xga[i]+eps
-#   ga1[i] <- negloglik_penalty(xa_0, X, y, S, lambda = 5e-5)
-# }
-# fd <- (ga1-ga0)/eps
-# fd1 <- grad_negloglik_penalty(xga, X, y, S, lambda = 5e-5)
-# fd1-fd
 
 #task3
-K <- ncol(X)
-init_gamma <- rep(0, K)
-opt <- optim(
-  par = init_gamma,
-  fn = negloglik_penalty,
-  gr = grad_negloglik_penalty,
-  method = "BFGS",
-  control = list(maxit = 200, trace = TRUE),
-  X = X, y = y, S = S, lambda = 5e-5
-)
+fit_death <- function(y, X, Xtilde, S, lambda = 5e-5, maxit = 200) {
+  # 初始化
+  K <- ncol(X)
+  init_gamma <- rep(0, K)
+  
+  # 使用 BFGS 优化
+  opt <- optim(
+    par = init_gamma,
+    fn = negloglik_penalty,
+    gr = grad_negloglik_penalty,
+    method = "BFGS",
+    control = list(maxit = maxit, trace = TRUE),
+    X = X, y = y, S = S, lambda = lambda
+  )
+  
+  if (opt$convergence != 0) {
+    warning("Optimization did not converge. Try adjusting lambda or maxit.")
+  }
+  
+  # 计算估计值
+  gamma_hat <- opt$par
+  beta_hat  <- exp(gamma_hat)
+  mu_hat    <- as.vector(X %*% beta_hat)
+  f_hat     <- as.vector(Xtilde %*% beta_hat)
+  
+  return(list(
+    opt = opt,
+    gamma_hat = gamma_hat,
+    beta_hat = beta_hat,
+    mu_hat = mu_hat,
+    f_hat = f_hat,
+    lambda = lambda
+  ))
+}
 
-opt$convergence  # 0 表示收敛
-gamma_hat <- opt$par
-beta_hat <- exp(gamma_hat)
-mu_hat <- as.vector(X %*% beta_hat)
-f_hat <- as.vector(Xtilde %*% beta_hat)
 
-# 计算所有数据的范围，确保y轴能容纳三条曲线
-y_all <- range(c(y, mu_hat, f_hat))
+plot_death <- function(t_obs, y, tf, out, mu_hat, f_hat, lambda = NULL) {
+  y_all <- range(c(y, mu_hat, f_hat))
+  
+  # 绘制实际死亡点
+  plot(t_obs, y, type = "p", col = "grey",
+       main = "Observed, Fitted Deaths and Estimated Infections",
+       xlab = "Time (days)", ylab = "Count",
+       ylim = y_all)
+  # 添加拟合死亡曲线
+  lines(t_obs, mu_hat, col = "blue", lwd = 2)
+  
+  # 添加感染曲线
+  lines(out$tf, f_hat, col = "red", lwd = 2)
+  
+  # 添加图例
+  legend("topleft",
+         legend = c("Observed Deaths", "Fitted Deaths", "Estimated Infections"),
+         col = c("grey", "blue", "red"),
+         lty = c(NA, 1, 1), pch = c(1, NA, NA),
+         bty = "n")  # 无边框图例
+  
+  # 若想标出观测期起点：
+  abline(v = t_obs[1], lty = 2, col = "darkgrey")
+  
+}
 
-# 绘图
-plot(t_obs, y, type = "p", col = "grey",
-     main = "Observed, Fitted Deaths and Estimated Infections",
-     xlab = "Time (days)", ylab = "Count",
-     ylim = y_all)
+#task4
+log_lambda_interval <- seq(-13,-7,length=50)
+bic_criterion <- function(X,S,y,log_lambda_interval){
+  n <- length(y)
+  K <- ncol(X)
+  init_gamma <- rep(0, K)
+  bic_interval <- numeric(0)
+  lambda_interval <- exp((log_lambda_interval))
+  for (lam in lambda_interval){
+    opt <- optim(
+      par = init_gamma,
+      fn = negloglik_penalty,
+      gr = grad_negloglik_penalty,
+      method = "BFGS",
+      control = list(maxit = 200, trace = TRUE),
+      X = X, y = y, S = S, lambda = lam
+    )
+    init_gamma <-opt$par
+    beta <- exp(opt$par)
+    mu <- pmax(as.vector(X %*% beta), 1e-10)
+    loglik <- sum(y * log(mu) - mu)
+    w <- as.numeric((y/(mu^2)))
+    H_lambda <- t(X) %*% (X * w) + lam*S
+    H_lambda <- H_lambda+diag(1e-8, ncol(X))
+    H_0 <-  t(X) %*% (X * w)
+    edf <- sum(diag(solve(H_lambda,H_0)))
+    bic <- -2*loglik + log(n)*edf
+    bic_interval <- c(bic_interval,bic)
+  }
+  lambda_hat <- lambda_interval[which.min(bic_interval)]
+  return(lambda_hat)
+}
 
-# 添加拟合死亡曲线
-lines(t_obs, mu_hat, col = "blue", lwd = 2)
+#task5
+w_negloglik_penalty <- function(gamma, wb, X, y, S, lambda) {
+  beta <- exp(gamma)              
+  mu <- as.vector(X %*% beta)       
+  mu <- pmax(mu, 1e-10)         
+  
+  loglik <- sum(wb*(y * log(mu) - mu))  # no log(yi!),after derivation, it eauals to 0
+  penalty <- 0.5 * lambda * t(beta) %*% S %*% beta
+  
+  return(-loglik + penalty)     
+}
 
-# 添加感染曲线
-lines(out$tf, f_hat, col = "red", lwd = 2)
+w_grad_negloglik_penalty <- function(gamma, wb, X, y, S, lambda){
+  beta <- exp(gamma)
+  mu <- as.vector(X %*% beta)
+  mu <- pmax(mu, 1e-10)
+  grad_beta <- t(X) %*% (wb*(1 - y / mu))
+  grad_gamma <- as.vector(beta * grad_beta + lambda * (beta * (S %*% beta)))
+  return(grad_gamma)
+}
 
-# 添加图例
-legend("topleft",
-       legend = c("Observed Deaths", "Fitted Deaths", "Estimated Infections"),
-       col = c("grey", "blue", "red"),
-       lty = c(NA, 1, 1), pch = c(1, NA, NA),
-       bty = "n")  # 无边框图例
+bootstrap <- function(X, Xtilde, S, y, log_lambda_interval){
+  lambda_hat <- bic_criterion(X,S,y,log_lambda_interval)
+  
+  n <- length(y)
+  K <- ncol(X)
+  
+  wb_mat <- replicate(200, tabulate(sample(n, replace = TRUE), n))
+  
+  fhat_boot <- matrix(NA, nrow = nrow(Xtilde), ncol = 200)
+  
+  for(i in 1:200){
+    wb <- wb_mat[, i]
+    opt <- opt <- optim(
+      par = rep(0, K),
+      fn = w_negloglik_penalty,
+      gr = w_grad_negloglik_penalty,
+      method = "BFGS",
+      control = list(maxit = 200, trace = FALSE),
+      X = X, y = y, S = S, lambda = lambda_hat, wb = wb
+    )
+    beta_i <- exp(opt$par)
+    fhat_boot[, i] <- as.vector(Xtilde %*% beta_i)
+  }
+  return(list(
+    fhat_boot = fhat_boot,
+    lambda_hat = lambda_hat,
+    wb_mat = wb_mat
+  ))
+}
 
-# 若想标出观测期起点：
-abline(v = t_obs[1], lty = 2, col = "darkgrey")
 
+#task6
+plot_bootstrap <- function(tf, fhat_boot) {
+  # 计算均值与置信区间
+  f_mean <- rowMeans(fhat_boot)
+  f_low  <- apply(fhat_boot, 1, quantile, probs = 0.025)
+  f_high <- apply(fhat_boot, 1, quantile, probs = 0.975)
+  
+  # 绘图
+  plot(tf, f_mean, type = "l", col = "red", lwd = 2,
+       main = "Estimated infection curve with 95% bootstrap CI",
+       xlab = "Time (days)", ylab = "f(t)")
+  
+  lines(tf, f_low, col = "grey60", lty = 2)
+  lines(tf, f_high, col = "grey60", lty = 2)
+  
+  # 灰色置信区间阴影
+  polygon(c(tf, rev(tf)), c(f_low, rev(f_high)),
+          col = rgb(0.9, 0.9, 0.9, 0.5), border = NA)
+}
+
+data <- read.table('D:/studying/Extended statistical programming/Assessment/engcov.txt', header = TRUE)
+
+# Generate X, S
+t_obs <- data$julian
+y <- data$nhs
+out<-Xtilde_X_S(t_obs)
+X <- out$X
+Xtilde <- out$Xtilde
+S <- out$S
+tf <- out$tf
+par(mfrow = c(2, 2))
+# 用固定 λ = 5e-5 拟合并绘图
+fit_result <- fit_death(y, X, Xtilde, S, lambda = 5e-5)
+plot_death(t_obs, y, tf, out, fit_result$mu_hat, fit_result$f_hat, lambda = fit_result$lambda)
+
+# BIC 搜索最优 λ 
+log_lambda_interval <- seq(-13, -7, length = 50)
+lambda_hat <- bic_criterion(X, S, y, log_lambda_interval)
+cat("Best lambda found by BIC:", lambda_hat, "\n")
+
+# 用最优 λ 重新拟合并绘图
+fit_best <- fit_death(y, X, Xtilde, S, lambda = lambda_hat)
+plot_death(t_obs, y, tf, out, fit_best$mu_hat, fit_best$f_hat, lambda = lambda_hat)
+
+# Bootstrap 重采样计算置信区间
+boot_result <- bootstrap(X, Xtilde, S, y, log_lambda_interval)
+fhat_boot <- boot_result$fhat_boot
+lambda_hat <- boot_result$lambda_hat
+cat("Bootstrap finished, using lambda =", lambda_hat, "\n")
+
+# 绘制感染曲线及 95% 置信带
+plot_bootstrap(tf, fhat_boot)
